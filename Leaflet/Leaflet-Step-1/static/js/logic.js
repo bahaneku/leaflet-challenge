5db2bb0787385b6ebdc45bc0e354928ae55b4163
turlin <boneth.ahaneku@cuanschutz.edu>
2020-10-13 18:47:00 -0600

    trying to add a satellite layer

--- Leaflet/Leaflet-Step-1/static/js/logic.js
+++ Leaflet/Leaflet-Step-1/static/js/logic.js
@@ -12,7 +12,7 @@ function createFeatures(earthquakeData) {
         layer.bindPopup("<h3" + feature.properties.place + "</h3><hr><p>" +new Date(feature.properties.time) + "</p>");
     }
     function radiusSize(magnitude) {
-        return magnitude * 5;
+        return magnitude * 2;
     }
 
     function circleColor(magnitude) {
@@ -54,27 +54,37 @@ function style (feature) {
     createMap(earthquakes);
 };
 function createMap(earthquakes) {
-    // Streetmap and darkmap layers
-    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    // Streetmap layer
+    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
         tileSize: 512,
         maxZoom: 18,
         zoomOffset: -1,
-        id: "mapbox/streets-v11",
+        id: "streets-v11",
         accessToken: API_KEY
     });
-
+    
+    // Darkmap layer
     var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
         maxZoom: 18,
         id: "dark-v10",
         accessToken: API_KEY
     });
+    
+    // Satellite layer
+    var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        maxZoom: 9,
+        id: "satellite-streets-v11",
+        accessToken: API_KEY
+    });
 
     // baseMaps layer
     var baseMaps = {
         "Street Map": streetmap,
-        "Dark Map": darkmap
+        "Dark Map": darkmap,
+        "Satellite": satellite
     };
 
     // Overlay layer
@@ -96,17 +106,18 @@ function createMap(earthquakes) {
         collapsed: false
     }).addTo(myMap);
 
-    // // Create a legend to display information about our map
-    var info = L.control({
-        position: "bottomright"
-    });
+    // // // Create a legend to display information about our map
+    // var legend = L.control({
+    //     position: "bottomright"
+    // });
 
-    // // // When the layer control is added, insert a div with the class of "legend"
-    info.onAdd = function () {
-        var div = L.DomUtil.create("div", "legend");
-        return div;
-    };
-    // // Add the info legend to the map
-    info.addTo(map);
+    // // // // When the layer control is added, insert a div with the class of "legend"
+    // legend.onAdd = function (map) {
+    //     var div = L.DomUtil.create("div", "info legend"),
+        // mags = [""];
+    //     return div;
+    // };
+    // // // Add the info legend to the map
+    // info.addTo(map);
 };
 
